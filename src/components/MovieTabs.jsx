de6f6aeed01d0e0e7b9328cddc42bc1d15e0b1e4
MaksymI <maksym.izmailov.lv@gmail.com>
2019-11-02 14:08:44 +0200

    classNames lib usage

--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from 'classnames';
 
 class MovieTabs extends React.Component {
   shouldComponentUpdate(nextProps) {
@@ -12,36 +13,36 @@ class MovieTabs extends React.Component {
   render() {
     const { sort_by, updateSortBy } = this.props;
     const handleClick = value => () => updateSortBy(value);
-    const getClassLink = value => `nav-link ${sort_by === value ? "active" : ""}`;
+    // const getClassLink = value => `nav-link ${sort_by === value ? "active" : ""}`;
+    const tabClass = value => classNames({
+        'nav-link': true,
+        'active': sort_by === value
+    });
     return (
       <ul className="tabs nav nav-pills">
         <li className="nav-item">
           <div
-            className={`nav-link ${
-              sort_by === "popularity.desc" ? "active" : ""
-            }`}
-            className={getClassLink("popularity.desc")}
+            // className={getClassLink("popularity.desc")}
             onClick={handleClick("popularity.desc")}
+            className={tabClass("popularity.desc")}
           >
             Popularity desc
           </div>
         </li>
         <li className="nav-item">
           <div
-            className={getClassLink("revenue.desc")}
+            // className={getClassLink("revenue.desc")}
             onClick={handleClick("revenue.desc")}
-            className={`nav-link ${sort_by === "revenue.desc" ? "active" : ""}`}
+            className={tabClass("revenue.desc")}
           >
             Revenue desc
           </div>
         </li>
         <li className="nav-item">
           <div
-            className={getClassLink("vote_average.desc")}
+            // className={getClassLink("vote_average.desc")}
             onClick={handleClick("vote_average.desc")}
-            className={`nav-link ${
-              sort_by === "vote_average.desc" ? "active" : ""
-            }`}
+            className={tabClass("vote_average.desc")}
           >
             Vote avetage desc
           </div>
